d4e7141b7e5dafa317e4a023dfaa4d556f00cbca
YellowGreenLime <nlime3141592@naver.com>
2025-12-12 20:56:09 +0900

    Diamond Square 알고리즘 구현 완료

--- DiamondSquare.cs
+++ DiamondSquare.cs
@@ -23,18 +23,25 @@ namespace nl
 
         private Random _prng;
         private float _gaussScale;
+        private float _h;
+
+        private float _vMin;
+        private float _vMax;
 
         public DiamondSquare(
             int resolution,
             float gaussScale,
+            float h,
             float r0, float g0, float b0,
             float r1, float g1, float b1)
         {
             Debug.Assert(resolution > 0 && resolution < 12, "Invalid image size.");
+            Debug.Assert(gaussScale >= 0.0f && gaussScale <= 1.0f);
+            Debug.Assert(h > 0.0f && h < 1.0f);
 
             _resolution = resolution;
-            _size = 1 << (_resolution + 1) + 1;
-
+            _size = (1 << _resolution) + 1;
+ 
             _map = new float[_size, _size];
 
             this.r0 = r0;
@@ -47,6 +54,10 @@ namespace nl
 
             _prng = new Random();
             _gaussScale = gaussScale;
+            _h = h;
+
+            _vMin = float.MaxValue;
+            _vMax = float.MinValue;
         }
 
         public void Create()
@@ -58,92 +69,109 @@ namespace nl
             _map[s,0] = _prng.NextSingle();
             _map[s,s] = _prng.NextSingle();
 
-            Create_Rec(0, 0, s, s, 1);
+            int scale = 1 << _resolution;
+            float depth = 1.0f;
+            float depthMultiplier = MathF.Pow(2, -_h);
+
+            while (scale > 1)
+            {
+                int nextScale = scale / 2;
+
+                CreateDiamond(nextScale, scale, depth);
+                CreateSquare(0, nextScale, depth);
+
+                scale = nextScale;
+                depth *= depthMultiplier;
+            }
         }
 
-        private void Create_Rec(int x0, int y0, int x1, int y1, float depth)
+        private void CreateDiamond(int offset, int scale, float depth)
         {
-            if (x0 + 1 == x1)
-                return;
-
-            int xm = (x0 + x1) / 2;
-            int ym = (y0 + y1) / 2;
-
-            CreateDiamond(x0, y0, x1, y1, xm, ym, depth);
-            CreateSquare(x0, y0, x1, y1, xm, ym, depth);
-
-            float depth2 = depth * 0.5f;
-
-            Create_Rec(x0, y0, xm, ym, depth2);
-            Create_Rec(xm, y0, x1, ym, depth2);
-            Create_Rec(x0, ym, xm, y1, depth2);
-            Create_Rec(xm, ym, x1, y1, depth2);
+            for (int x = offset; x < _size; x += scale)
+            {
+                for (int y = offset; y < _size; y += scale)
+                {
+                    _map[x, y] = GetDiamondValue(x, y, offset) + GaussRandom() * _gaussScale * depth;
+                }
+            }
         }
 
-        private void CreateDiamond(int x0, int y0, int x1, int y1, int xm, int ym, float depth)
+        private void CreateSquare(int offset, int scale, float depth)
         {
-            _map[xm, ym] = GetAvg4(
-                _map[x0, y0],
-                _map[x1, y0],
-                _map[x0, y1],
-                _map[x1, y1],
-                depth
-            );
+            for (int x = offset; x < _size; x += scale)
+            {
+                for (int y = 0; y < _size; y += scale)
+                {
+                    _map[x, y] = GetSquareValue(x, y, scale) + GaussRandom() * _gaussScale * depth;
+                    _map[y, x] = GetSquareValue(y, x, scale) + GaussRandom() * _gaussScale * depth;
+                }
+            }
         }
 
-        private void CreateSquare(int x0, int y0, int x1, int y1, int xm, int ym, float depth)
+        private float GetDiamondValue(int x, int y, int scale)
         {
-            _map[x0, ym] = GetAvg3(
-                _map[x0, y0],
-                _map[xm, ym],
-                _map[x0, y1],
-                depth
-            );
+            float count = 0.0f;
+            float value = 0.0f;
+            float v;
 
-            _map[xm, y0] = GetAvg3(
-                _map[x0, y0],
-                _map[xm, ym],
-                _map[x1, y0],
-                depth
-            );
+            if (TryGetValue(out v, x + scale, y + scale)) { count += 1.0f; value += v; }
+            if (TryGetValue(out v, x + scale, y - scale)) { count += 1.0f; value += v; }
+            if (TryGetValue(out v, x - scale, y + scale)) { count += 1.0f; value += v; }
+            if (TryGetValue(out v, x - scale, y - scale)) { count += 1.0f; value += v; }
 
-            _map[x1, ym] = GetAvg3(
-                _map[x1, y0],
-                _map[xm, ym],
-                _map[x1, y1],
-                depth
-            );
-
-            _map[xm, y1] = GetAvg3(
-                _map[x0, y1],
-                _map[xm, ym],
-                _map[x1, y1],
-                depth
-            );
+            return value / count;
         }
 
-        private float GetAvg3(float v0, float v1, float v2, float depth)
+        private float GetSquareValue(int x, int y, int scale)
         {
-            float avg = (v0 + v1 + v2) / 3.0f;
+            float count = 0.0f;
+            float value = 0.0f;
+            float v;
 
-            return avg + GaussRandom(depth);
+            if (TryGetValue(out v, x + scale, y)) { count += 1.0f; value += v; }
+            if (TryGetValue(out v, x - scale, y)) { count += 1.0f; value += v; }
+            if (TryGetValue(out v, x, y + scale)) { count += 1.0f; value += v; }
+            if (TryGetValue(out v, x, y - scale)) { count += 1.0f; value += v; }
+
+            return value / count;
         }
 
-        private float GetAvg4(float v0, float v1, float v2, float v3, float depth)
+        private bool TryGetValue(out float v, int x, int y)
         {
-            float avg = (v0 + v1 + v2 + v3) / 4.0f;
+            if (x < 0 || x >= _size || y < 0 || y >= _size)
+            {
+                v = 0.0f;
+                return false;
+            }
 
-            return avg + GaussRandom(depth);
+            v = _map[x, y];
+            return true;
         }
 
-        private float GaussRandom(float depth)
+        private void CompareMin(float v)
+        {
+            if (v < _vMin)
+            {
+                _vMin = v;
+            }
+        }
+
+        private void CompareMax(float v)
+        {
+            if (v > _vMax)
+            {
+                _vMax = v;
+            }
+        }
+
+        private float GaussRandom()
         {
             float u1 = 1.0f - _prng.NextSingle();
             float u2 = 1.0f - _prng.NextSingle();
 
             float randStdNormal = MathF.Sqrt(-2.0f * MathF.Log(u1)) * MathF.Sin(2.0f * MathF.PI * u2);
 
-            return randStdNormal * _gaussScale * depth;
+            return randStdNormal;
         }
 
         public string Save()
@@ -160,6 +188,18 @@ namespace nl
             string path = $@"C:\Test\{dateString}.png";
 
             PngBuilder builder = PngBuilder.Create(_size, _size, false);
+
+            _vMin = float.MaxValue;
+            _vMax = float.MinValue;
+
+            for (int x = 0; x < _size; ++x)
+            {
+                for (int y = 0; y < _size; ++y)
+                {
+                    CompareMin(_map[x, y]);
+                    CompareMax(_map[x, y]);
+                }
+            }
 
             for (int x = 0; x < _size; ++x)
             {
@@ -179,15 +219,42 @@ namespace nl
 
         private Pixel GetPixel(int x, int y)
         {
-            // float r = r0 + (r1 - r0) * _map[x, y];
-            // float g = g0 + (g1 - g0) * _map[x, y];
-            // float b = b0 + (b1 - b0) * _map[x, y];
+            float v = 0.0f;
 
-            float r = r0 + (r1 - r0) * MathF.Max(0.0f, MathF.Min(1.0f, _map[x, y]));
-            float g = g0 + (g1 - g0) * MathF.Max(0.0f, MathF.Min(1.0f, _map[x, y]));
-            float b = b0 + (b1 - b0) * MathF.Max(0.0f, MathF.Min(1.0f, _map[x, y]));
+            v = GetNorm(_map[x, y]);
+            v = Clamp(v, 0.0f, 1.0f) * 255.0f;
+
+            float r = v;
+            float g = v;
+            float b = v;
+
+            // float r = r0 + (r1 - r0) * MathF.Max(0.0f, MathF.Min(1.0f, _map[x, y].value));
+            // float g = g0 + (g1 - g0) * MathF.Max(0.0f, MathF.Min(1.0f, _map[x, y].value));
+            // float b = b0 + (b1 - b0) * MathF.Max(0.0f, MathF.Min(1.0f, _map[x, y].value));
 
             return new Pixel((byte)r, (byte)g, (byte)b);
+        }
+
+        private float Clamp(float v, float min, float max)
+        {
+            return MathF.Max(min, MathF.Min(max, v));
+        }
+
+        private float GetNorm(float v)
+        {
+            return (v - _vMin) / (_vMax - _vMin);
+        }
+
+        private bool IsSingleBit(int value)
+        {
+            int state = 0;
+
+            for (int i = 0; i < sizeof(int) * 8; ++i)
+            {
+                state += (value & 0x1);
+            }
+
+            return state == 1;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,13 +7,16 @@ namespace nl
         private static void Main(string[] args)
         {
             DiamondSquare ds = new DiamondSquare(
-                8,
-                0.1f,
+                resolution: 8,
+                gaussScale: 0.1f,
+                h: 0.9f,
                 252.0f, 62.0f, 133.0f,
                 9.0f, 107.0f, 239.0f
             );
 
-            ds.Create();
+            for (int i = 0; i < 1; ++i)
+                ds.Create();
+            
             ds.Save();
         }
     }
